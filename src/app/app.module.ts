9ce0c1b257f62c60ff04e3d7f86e689cc98bd58a
EC2 Default User <ec2-user@ip-172-31-27-33.ec2.internal>
2019-04-12 07:30:12 +0000

    ES02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormDemoComponent } from './form-demo/form-demo.component';
+
+import {   //Importare le funzionalità nella nostra applicazione
+ FormsModule,
+ ReactiveFormsModule
+} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,  //Anche qui
+   ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
